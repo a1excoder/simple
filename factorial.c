7bd2eea84a1f71d49420f9a2c11a28e1c2a50e89
Aleksandr Rudenko <49398367+a1excoder@users.noreply.github.com>
2021-10-26 11:07:20 +0300

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int factorial(int n) {
+	if (n < 1) {
+		return 1;
+	}
+	return n * factorial(n-1);
+}
+
+int main(void) {
+	int z = 0;
+
+	while(1) {
+		printf("Enter integer: ");
+		scanf("%d", &z);
+		if (z == 0) break;
+		printf("factorial(%d) = %d\n", z, factorial(z));
+	}
+}
