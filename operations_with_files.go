eab9af2e3748a61a9083c81235ca29144bece937
Aleksandr Rudenko <49398367+a1excoder@users.noreply.github.com>
2021-10-27 20:21:55 +0300

    Create operations_with_files.go

---
+++ operations_with_files.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io"
+)
+
+func Write(fname, query string) error {
+	file, err := os.OpenFile(fname, os.O_WRONLY, 0666)
+	if (err != nil) {
+		return err
+	}
+	defer file.Close()
+	buffer := []byte(query)
+	file.Write(buffer)
+
+	return nil
+}
+
+func Create(fname string) error {
+	file, err := os.Create(fname)
+	if (err != nil) {
+		return err
+	}
+
+	file.Close()
+	return nil
+}
+
+func Read(fname string, buffer_size int, channel chan <- []byte) error {
+	file, err := os.OpenFile(fname, os.O_RDONLY, 0666)
+	if (err != nil) {
+		channel <- []byte(fmt.Sprintf("[ERROR] - %s\n", err))
+		close(channel)
+		return err
+	}
+	defer file.Close()
+	defer close(channel)
+
+	buffer := make([]byte, buffer_size)
+
+	for {
+		len_str, err := file.Read(buffer)
+		if (err == io.EOF) {
+			break
+		}
+		channel <- buffer[:len_str]
+	}
+	channel <- []byte("\n")
+
+	return nil
+}
+
+func main() {
+	GetName := func() (result string) {
+		fmt.Print("Enter file name: ")
+		fmt.Fscan(os.Stdin, &result)
+
+		return
+	}
+	var option int8
+
+	for {
+		fmt.Print("read from file: `1` write: `2` create `3` exit `0`: ")
+		fmt.Fscan(os.Stdin, &option)
+		
+
+		switch (option) {
+		case 1:
+			channel := make(chan []byte)
+			go Read(GetName(), 256, channel)
+
+			for {
+				data, opened := <- channel
+				if (!opened) {
+					break
+				}
+				fmt.Print(string(data))
+			}
+		case 2:
+			fname := GetName()
+			var data string
+
+			fmt.Print("Enter data: ")
+			fmt.Fscan(os.Stdin, &data) // catch only one word
+			err := Write(fname, data)
+
+			if (err != nil) {
+				fmt.Printf("[ERROR] - %s\n", err)
+			}
+		case 3:
+			err := Create(GetName())
+			if (err != nil) {
+				fmt.Printf("[ERROR] - %s\n", err)
+			}
+		case 0:
+			os.Exit(0)
+		}
+	}
+	
+}
