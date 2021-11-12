35efbc644e654403f1caa980da2ee984a544fc7b
Aleksandr Rudenko <49398367+a1excoder@users.noreply.github.com>
2021-11-12 09:38:51 +0200

    Update get_file_type.c

--- get_file_type.c
+++ get_file_type.c
@@ -21,7 +21,7 @@ static enum TYPES check_type_by_str(const char *type) {
 	return ANOTHER;
 }
 
-enum TYPES get_file_name(const char *full_name) {
+enum TYPES get_file_type(const char *full_name) {
 	int size_to_dot, strsize = 0;
 	char c, cc;
 	while ((c = *(full_name++)) != '\0') {
@@ -36,7 +36,7 @@ enum TYPES get_file_name(const char *full_name) {
 	char str_type[strsize - size_to_dot];
 
 	for (int i = size_to_dot, j = 0; i < strsize; i++, j++) {
-		str_type[j] = *((full_name - strsize-1) + i);
+		str_type[j] = *((full_name - strsize) + i-1); // str_type[j] = *((full_name - strsize-1) + i);
 	}
 
 	return check_type_by_str(str_type);
@@ -44,24 +44,24 @@ enum TYPES get_file_name(const char *full_name) {
 
 int main(void)
 {
-	char fname[] = "test.bla";
-	if (get_file_name(fname) == TXT) {
+	char fname[] = "test.mp4";
+	if (get_file_type(fname) == TXT) {
 		printf("file type is txt\n");
 	}
 
-	if (get_file_name(fname) == PNG) {
+	if (get_file_type(fname) == PNG) {
 		printf("file type is png\n");
 	}
 
-	if (get_file_name(fname) == JPG) {
+	if (get_file_type(fname) == JPG) {
 		printf("file type is jpg\n");
 	}
 
-	if (get_file_name(fname) == MP4) {
+	if (get_file_type(fname) == MP4) {
 		printf("file type is mp4\n");
 	}
 
-	if (get_file_name(fname) == ANOTHER) {
+	if (get_file_type(fname) == ANOTHER) {
 		printf("file type is another\n");
 	}
 
